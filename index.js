26778fc985018f8fad692960f9007adf4d9ab1ba
Fellipe Chagas <chagas@fellipes-air.home>
2017-04-09 17:35:39 -0300

    Refactor scrapper runner

--- index.js
+++ index.js
@@ -4,47 +4,17 @@ const Scrapper = require('./src/services/Scrapper')
 const PlayersStatsConfig = require('./src/configs/PlayersStats')
 const PlayersStats = require('./src/repositories/PlayersStats')
 const CsvExporter = require('./src/services/CsvExporter')
+const Runner = require('./src/services/Runner')
 
-console.log('Started...')
-
-const scrapper = new Scrapper(PlayersStatsConfig, scrapeIt)
-const repository = new PlayersStats(CsvExporter)
-
-let offset = 0
-let promises = []
-
-let _delay = (t) => {
-  return new Promise(function(resolve) {
-    setTimeout(resolve, t)
-  })
-}
-
-const _scrap = (year, offset) => {
-  console.log(`Running with year: ${year} and ${offset} offset...`)
-  return scrapper.start({ year: year, offset: offset }).then((result) => {
-    console.log(`Scrapped ${result.items.length} items...`)
-    if (result.items.length == 0) {
-      return Promise.resolve(true)
-    }
-    console.log('Adding items to repository...')
-    result.items.map(repository.add.bind(repository))
-    offset += 100
-
-    return _delay(5000).then(() => {
-      _scrap(year, offset)
-    })
-  })
-}
-
-for(let year = 1984; year <= 2016; year++) {
-  offset = 0
-  promises.push(
-    _scrap(year, offset)
-  )
-}
-
-Promise.all(promises).then(() => {
-  repository.export().then(() => {
-    console.log('Done!')
-  })
+const runner = new Runner({
+  scrapeIt: scrapeIt,
+  config: PlayersStatsConfig,
+  Exporter: CsvExporter,
+  Scrapper: Scrapper,
+  Repository: PlayersStats
 })
+
+const startYear = 1984
+const endYear = 2016
+
+runner.run(startYear, endYear).then(() => console.log('Done! (:'))
---
+++ src/services/Runner.js
@@ -0,0 +1,41 @@
+class Runner {
+  constructor (params) {
+    this.offset = 0
+    this.promises = []
+    this.scrapper = new params.Scrapper(params.config, params.scrapeIt)
+    this.repository = new params.Repository(params.Exporter)
+  }
+
+  run (startYear, endYear) {
+    for (let year = startYear; year <= endYear; year++) {
+      this.promises.push(
+        this._scrap(year)
+      )
+    }
+
+    Promise.all(this.promises).then(() => {
+      return this.repository.export()
+    })
+  }
+
+  _scrap (year, offset = 0) {
+    console.log(`Running for year: ${year} and ${offset} offset...`)
+    const params = { year: year, offset: offset }
+    return this.scrapper.start(params).then((result) => {
+      if (!result.items.length) {
+        return Promise.resolve(true)
+      }
+
+      this._addItemsToRepository(result.items)
+
+      return this._scrap(year, offset + 100)
+    })
+  }
+
+  _addItemsToRepository (items) {
+    console.log(`Adding ${items.length} items to repository...`)
+    items.map(this.repository.add.bind(this.repository))
+  }
+}
+
+module.exports = Runner
